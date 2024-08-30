ad504abc6a557691026f41f40302f434c1e7122f
Newsnownaija <143800421+Newsnownaija@users.noreply.github.com>
2024-08-30 16:21:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,62 @@
-const EVENTS_DELAY = 20000;
 const defaultLanguage = document.documentElement.getAttribute('lang')
 const gamePromoConfigs = {
-    MyCloneArmy: {
-        appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
-        promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767'
-    },
     ChainCube2048: {
         appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
-        promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3'
+        promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
+        eventsDelay: 20000,
+        attemptsNumber: 10
     },
     TrainMiner: {
         appToken: '82647f43-3f87-402d-88dd-09a90025313f',
-        promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954'
+        promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
+        eventsDelay: 20000,
+        attemptsNumber: 10
     },
-    BikeRide3D: {
-        appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
-        promoId: '43e35910-c168-4634-ad4f-52fd764a843f'
+    MergeAway: {
+        appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
+        promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
+        eventsDelay: 20000,
+        attemptsNumber: 10
     },
+    TwerkRace: {
+        appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
+        promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
+        eventsDelay: 20000,
+        attemptsNumber: 10
+    },
+    Polysphere: {
+        appToken: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
+        promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
+        eventsDelay: 20000,
+        attemptsNumber: 20
+    },
+    MowandTrim: {
+        appToken: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+        promoId: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+        eventsDelay: 20000,
+        attemptsNumber: 20
+    },
+    CafeDash: {
+        appToken: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
+        promoId: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
+        eventsDelay: 23000,
+        attemptsNumber: 16
+    },
+    GangsWars: {
+        appToken: 'b6de60a0-e030-48bb-a551-548372493523',
+        promoId: 'c7821fa7-6632-482c-9635-2bd5798585f9',
+        eventsDelay: 40000,
+        attemptsNumber: 23
+    }, 
+    Zoopolis: {
+        appToken: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
+        promoId: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
+        eventsDelay: 21000,
+        attemptsNumber: 23
+    }
 };
 
-let currentAppConfig = gamePromoConfigs.MyCloneArmy;
+let currentAppConfig = Object.values(gamePromoConfigs)[0];
 var currentLanguage;
 var keygenActive = false;
 
@@ -149,11 +186,11 @@ document.getElementById('startBtn').addEventListener('click', async () => {
             startBtn.disabled = false;
             return null;
         }
-
-        for (let i = 0; i < 7; i++) {
-            await sleep(EVENTS_DELAY * delayRandom());
+        
+        for (let i = 0; i < currentAppConfig.attemptsNumber; i++) {
+            await sleep(currentAppConfig.eventsDelay * delayRandom());
             const hasCode = await emulateProgress(clientToken);
-            updateProgress(10 / keyCount);
+            updateProgress((100 / currentAppConfig.attemptsNumber) / keyCount);
             if (hasCode) {
                 break;
             }
@@ -161,7 +198,6 @@ document.getElementById('startBtn').addEventListener('click', async () => {
 
         try {
             const key = await generateKey(clientToken);
-            updateProgress(30 / keyCount);
             return key;
         } catch (error) {
             alert(`Failed to generate key: ${error.message}`);
@@ -235,15 +271,7 @@ document.getElementById('startBtn').addEventListener('click', async () => {
 });
 
 document.getElementById('creatorChannelBtn').addEventListener('click', () => {
-    const base64Url = 'aHR0cHM6Ly9iaXQubHkvY3J5cHRvd29ybGRhemU=';
-    const decodedUrl = atob(base64Url);
-    window.location.href = decodedUrl;
-});
-
-document.getElementById('creatorGameBtn').addEventListener('click', () => {
-    const base64Url = 'aHR0cHM6Ly9iaXQubHkvYXZybzIwMjRf';
-    const decodedUrl = atob(base64Url);
-    window.location.href = decodedUrl;
+    window.location.href = 'https://t.me/pdosi_project';
 });
 
 function generateClientId() {
